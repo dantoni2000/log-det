04ead85502dce482e8ea96c460146f7a275c71a0
danitoni2000 <daniele.toni@sns.it>
2025-12-20 22:41:33 +0100

    Add files via upload

--- bounds_comparison.m
+++ bounds_comparison.m
@@ -5,7 +5,7 @@ close all
 warning off
 
 % number of the experiment
-c = 2;
+c = 1;
 
 % dimension of the matrix
 n = 4000;
@@ -18,20 +18,29 @@ n = 4000;
 l_min = 100; l_max = 1000; step = 100; lgt = 1 + (l_max - l_min) / step;
 
 % iterations of Lanczos
-m = 10;
+m = 50;
 
 
 % computation of the bounds: takes the minimum over all possible choices of p and k
 for l =l_min:step:l_max
-    l
+    l + m
+    
     for s = 2:2:l-2
         k = s;
         p = l - k;
-        squareboundTr(:,s/2) = (k+p+m-1)/(p-1) * sum(log(1+diag(G(k+m+1:n,k+m+1:n))));
-
+     
         squareboundFro(:,s/2) = sqrt(2*(k+p-1)/(p-1)) * sum(log(1+diag(G(k+1:n,k+1:n))))^0.5 *( log(1+ (1+2*k/(p-1))* G(k+1,k+1) + 2*exp(2)*(k+p)/(p^2-1) * sum(diag(G(k+1:n,k+1:n)))) )^0.5 ;
      
     end
+
+
+    for s = 2:2:l+m-2
+        k = s;
+        p = l + m - k;
+       
+        squareboundTr(:,s/2) = (k+p-1)/(p-1) * sum(log(1+diag(G(k+1:n,k+1:n))));
+
+     end
 
     BestErr_Tr(l/step) = min(squareboundTr');
     BestErr_Fro(l/step) = min(squareboundFro');
